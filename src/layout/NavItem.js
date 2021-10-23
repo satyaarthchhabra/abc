3bfa32bd5d17e47b8a134331aea9411726e9d9b6
Satyaarth Chhabra <satyarthchhabra18@gmail.com>
2021-10-23 13:05:42 +0530

    error resolved

--- src/layout/NavItem.js
+++ src/layout/NavItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import clsx from "clsx";
-import PropTypes from "prop-types";
+
 import { Button, ListItem, makeStyles, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
